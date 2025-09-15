6397faa681bdd1a0764334d6750e9510dec5e278
so-groenen <solofo.groenendijk@gmail.com>
2025-09-15 18:03:50 +0200

    structure changed + created base classes

---
+++ experiment_base/experiment_handler.py
@@ -0,0 +1,161 @@
+from __future__ import annotations
+import numpy as np
+import os
+from typing import Protocol
+from .experiment_output import ExperimentOutput
+
+def array_to_str(array: np.ndarray, rounding: int) -> list[str]:
+    temps_str = []
+    for val in array:
+        temps_str.append(str(round(val,rounding)))
+    return ", ".join(temps_str)
+
+
+class ExperimentHandler(Protocol):
+    
+    def __init__(self, name: str, folder: str, lengths):
+        self.name: str                  = name
+        self.folder: str                = folder
+        self.lengths: np.ndarray        = lengths
+        self.param_files: dict          = dict()
+        self.out_files: dict            = dict()
+        self.__init_directory()
+        self.__set_files()
+    
+    def write_formated(self, L, rounding=3):
+        raise NotImplementedError()
+
+    def missing_parameters(self) -> dict:
+        raise NotImplementedError()
+    
+    def write_parameter_file(self, L: int, rounding = 3) -> None:
+        if len(self.missing_parameters()):
+            print(f">> Error Missing parameters: {self.missing_parameters()}")
+            return False
+
+        if self.param_files is None or self.out_files is None:
+            print("Parameter & output files not set!")
+        try:
+            self.write_formated(L, rounding)
+            print(f"-- {self.param_files[L]}: ", end="")
+            return True
+        except Exception as e:
+            print(f"Error occured: {e}, {e.args}")
+                
+    def write_parameter_files(self, temp_rounding=3):
+        print(f">> writing parameter files:")
+        for L in self.lengths:
+            try:
+                if self.write_parameter_file(L, temp_rounding):
+                    print("done")
+            except Exception as e:
+                print(f"Error occured: {e}, {e.args}")
+                
+    def __set_files(self):
+        print(">> Setting file paths:")
+        if self.lengths is None:
+            print(">> Set Files: Error: No lengths found!")
+            return
+
+        for L in self.lengths:
+            self.param_files[L] = f"{self.folder}/{self.name}/parameter_{L}x{L}.txt"
+            print(f"-- setting paramfile \"{self.param_files[L]}\"")
+        for L in self.lengths:
+            self.out_files[L]   = f"{self.folder}/{self.name}/out_{L}x{L}.txt"
+            print(f"-- setting outfile \"{self.out_files[L]}\"")
+    
+    def has_param_file(self, L: int) -> bool:
+        try:
+            with open(f"{self.folder}/{self.name}/parameter_{L}x{L}.txt", "r") as _:
+                pass
+            return True
+        except FileNotFoundError as _:
+            return False
+    
+    def are_parameter_files_available(self) -> bool:
+        has_all_files = True
+        missing_files = []
+        print(">> Looking for parameter files:")
+        if self.lengths is None:
+            print(f"* No lengths set")
+            return False
+        
+        for L in self.lengths:
+            file = f"\"{self.folder}/{self.name}/parameter_{L}x{L}.txt\""
+            if self.has_param_file(L):
+                print(f"-- Found parameter file: {file}")
+            else:
+                missing_files.append(file)
+                has_all_files = False
+                
+        if has_all_files:
+            print("* Parameter files available")
+        else:
+            print("* Not all parameter files created yet:")    
+            for files in missing_files:
+                print(f"-- not written yet: {files}")
+        return has_all_files
+     
+        
+    def are_results_available(self):
+        all_available = True
+        some_availabe = False
+        missing_files = []
+        print(">> Looking for output files:")
+        for L in self.lengths:
+            file = f"\"{self.folder}/{self.name}/out_{L}x{L}.txt\""
+            if self.has_output(L):
+                print(f"-- Found output: {file}")
+                some_availabe = True
+            else:
+                missing_files.append(file)
+                all_available = False
+                
+        if not all_available:
+            print(f"* missing output:")
+            for files in missing_files:
+                print(f"-- {files}")
+        if not some_availabe:
+            print("* No output files created yet.")
+            
+        return some_availabe
+        
+    def __init_directory(self):
+        try:
+            os.makedirs(f"{self.folder}/{self.name}")
+            print(f">> Directory \"{self.folder}/{self.name}\" created")
+        except FileExistsError:
+            print(f">> Directory \"{self.folder}/{self.name}\" Found.")
+        except OSError as e:
+            print(f"Error creating directory: {e}")
+
+    def has_output(self, L):
+        try:
+            with open(f"{self.folder}/{self.name}/out_{L}x{L}.txt", "r") as _:
+                pass
+            return True
+        except FileNotFoundError as e:
+            return False
+    
+    def set_result_type(self, output_file: str) -> ExperimentOutput:
+        raise NotImplementedError()
+
+        
+    def get_results(self) -> dict[int, ExperimentOutput]: #tuple[np.ndarray, dict[int, MonteCarloData]]:
+        print("")
+        has_results = False
+        results: dict[int, ExperimentOutput] = dict()
+
+        for L in self.lengths:
+            if self.has_output(L):
+                print(f"Found output: \"{self.folder}/{self.name}/out_{L}x{L}.txt")
+                has_results = True
+        if not has_results:
+            return None
+        
+        for L in self.lengths:
+            if self.has_output(L):
+                results[L] = self.set_result_type(f"{self.folder}/{self.name}/out_{L}x{L}.txt")
+                results[L].grab_files()
+
+        return results 
---
+++ monte_carlo_ising.py
@@ -0,0 +1,107 @@
+import numpy as np
+from experiment_base.experiment_handler import array_to_str, ExperimentHandler
+from experiment_base.experiment_output import ExperimentOutput
+
+def get_length_from_dicts(therm_steps: dict, measure_steps: dict):
+    lengths = []
+    for (L1, L2) in zip(therm_steps.keys(), measure_steps.keys()):
+        if L1 != L2:
+            return None
+        lengths.append(L1)
+    return lengths
+
+class MonteCarloData(ExperimentOutput):
+    def __init__(self, file_name):
+        super().__init__(file_name)
+
+        self.temperatures       = []
+        self.energy_density     = []
+        self.magnetisation      = []
+        self.specific_heat      = []
+        self.mag_susceptibility = []
+        self.elapsed_time       = -1
+        self.correlation_length = []
+
+    # Virtual function implementation:
+    def parse_output(self, line_number, lines):
+        if line_number == 0:
+            slines = lines.split(':')
+            try:
+                self.observables  = slines[0].split(', ')
+                self.elapsed_time = float(slines[1])
+            except Exception as _:
+                self.observables = lines.split(',')
+                print("No elasped time found.")
+        else:
+            slines = lines.split(", ")
+            self.temperatures.append(float(slines[0]))
+            self.energy_density.append(float(slines[1]))
+            self.magnetisation.append(float(slines[2]))
+            self.specific_heat.append(float(slines[3]))
+            self.mag_susceptibility.append(float(slines[4]))
+            self.correlation_length.append(float(slines[5]))
+            
+class MonteCarloExperiment(ExperimentHandler):
+    def __init__(self, name, folder, lengths):
+        
+        super().__init__(name, folder, lengths)
+        self.therm_steps   = None
+        self.measure_steps = None
+        self.temperatures  = None
+        self.measure_correlation_length = False
+        
+    # Virtual function implementation:
+    def set_result_type(self, output_file) -> ExperimentOutput:
+        return MonteCarloData(output_file)
+    
+    # Virtual function implementation:
+    def missing_parameters(self) -> list: 
+        missing = []
+        for (key, val) in vars(self).items():
+            if val is None:
+                missing.append(key)
+        return missing
+
+    # Virtual function implementation:
+    def write_formated(self, L, rounding=3):
+        with open(self.param_files[L], "w") as f:
+            f.write(f"rows: {L}\n")
+            f.write(f"cols: {L}\n")
+            f.write(f"therm_steps: {self.therm_steps[L]}\n")
+            f.write(f"measure_steps: {self.measure_steps[L]}\n")
+            f.write(f"temperatures: {array_to_str(self.temperatures, rounding)}\n")
+            f.write(f"measure_struct_fact: {self.measure_correlation_length}\n")
+            f.write(f"outputfile: {self.out_files[L]}\n")
+
+    def set_struct_fact_measure(self, val: bool):
+        self.measure_correlation_length = val
+        
+    def set_parameters(self, therm_steps: dict, measure_steps: dict):
+        param_lengths = get_length_from_dicts(therm_steps, measure_steps)
+        if param_lengths is None:
+            print(">> therm steps & measure steps should use same lengths")
+        if param_lengths != self.lengths:
+            print(">> Monte carlo parameter lengths do not match original lengths!")
+        else:
+            self.measure_steps = measure_steps
+            self.therm_steps   = therm_steps
+        print(">> Monte Carlo parameters set!")
+    
+    def set_temperatures(self, temps: np.ndarray):
+        if isinstance(temps, np.ndarray):
+            print(">> Temperatures set!")
+            self.temperatures = temps
+        else:
+            print(">> Temperatures not numpy array!")
+    
+    @staticmethod
+    def new_from_parameters(name: str, folder: str, therm_steps: dict, measure_steps: dict, temperatures: np.ndarray):
+        lengths = []
+        for (L, _) in zip(therm_steps.keys(), measure_steps.keys()):
+            lengths.append(L)
+                    
+        new_exp = MonteCarloExperiment(name, folder, lengths)
+        new_exp.set_parameters(therm_steps, measure_steps)
+        new_exp.set_temperatures(temperatures)
+        return new_exp
+        
