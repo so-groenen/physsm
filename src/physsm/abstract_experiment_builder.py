e62e2215e2ebd4f02ede55db530135d14ffb2d89
so-groenen <solofo.groenendijk@gmail.com>
2025-10-15 02:05:16 +0200

    replaced dataclass with classes

--- src/physsm/abstract_experiment_builder.py
+++ src/physsm/abstract_experiment_builder.py
@@ -27,58 +27,43 @@ class AbstractExperimentBuilder(Generic[OutType]):
         self.out_data                        = FileData(file_name="out", extension="txt")
         self.param_data                      = FileData(file_name="parameter", extension="txt")
         self.paths_data                      = PathData()
-        self.paths_data.proj_dir             = proj_dir
-        self.paths_data.target_dir           = self._get_target_dir(proj_dir, results_dir, exp_name, verbose_log)   
         self.paths_are_set                   = False
+        self.verbose_log                     = verbose_log
         self.experiment.verbose_log          = verbose_log
-
+        self.paths_data.set_proj_dir(proj_dir)
+        self.paths_data.set_target_dir(results_dir, exp_name, verbose_log)
+        
     def _log_path(self, path: Path) -> Path:
         if not self.verbose_log:
             return path.relative_to(self.paths_data.proj_dir)
         return path
-        
-    def _get_target_dir(self, proj_dir: Path, results_dir: str, exp_name: str, verbose = False):
-        _results_dir = proj_dir.joinpath(results_dir)
-        target_dir   = _results_dir.joinpath(exp_name)
-        if not proj_dir.exists():
-            raise NotADirectoryError(f">> Project Directory \"{self._log_path(proj_dir)}\" Not Found.")
-        else:
-            if not verbose:
-                print(f">> Project Directory \"{proj_dir.name}\" Found.")
-            else:
-                print(f">> Project Directory \"{proj_dir}\" Found.")
-                
-        if target_dir.exists():
-            print(f">> Directory \"{self._log_path(target_dir)}\" Found.")
-        else:
-            target_dir.mkdir(parents=True)
-            print(f">> Directory \"{self._log_path(target_dir)}\" created.")
-        return target_dir
-
+    
     def _set_files(self):
         print(">> Setting file paths:")
         if self.parameters.scale_variables is None:
             raise ValueError("set_files() Error: No scale variables")
         suffix = dict()
+
         out_name   = self.out_data.file_name
-        ext        = self.out_data.extension 
-        target_dir = self.paths_data.target_dir
+        out_ext    = self.out_data.extension 
         param_name = self.param_data.file_name
         param_ext  = self.param_data.extension
-        
+
         for L in self.parameters.scale_variables:
             suffix[L] = ",".join([f"{scale_name}={L}" for scale_name in self.parameters.scale_variable_names])
+
         for L in self.parameters.scale_variables:
-            self.paths_data.param_paths[L] = target_dir.joinpath(f"{param_name}_{suffix[L]}.{param_ext}")
-            print(f"-- setting paramfile \"{self._log_path(self.paths_data.param_paths[L])}\"")
-        for L in self.parameters.scale_variables:          
-            self.paths_data.out_paths[L] = target_dir.joinpath(f"{out_name}_{suffix[L]}.{ext}")
-            print(f"-- setting outfile \"{self._log_path(self.paths_data.out_paths[L])}\"")
+            param_path_name = f"{param_name}_{suffix[L]}.{param_ext}"
+            self.paths_data.set_param_path(L, param_path_name, self.verbose_log)
             
-        self.paths_are_set = True
+        for L in self.parameters.scale_variables:          
+            out_path_name = f"{out_name}_{suffix[L]}.{out_ext}"
+            self.paths_data.set_out_path(L, out_path_name, self.verbose_log)
+
         self.experiment.paths_data = self.paths_data
         self.experiment.parameters = self.parameters 
-    
+        self.paths_are_set = True
+
     def set_parameter_file_data(self, filename: str="parameter", extension = "txt"):
         self.param_data.set_extension(extension)
         self.param_data.set_output_name(filename)
@@ -94,61 +79,20 @@ class AbstractExperimentBuilder(Generic[OutType]):
     def set_scale_variable_names(self, names: list[str]):
         self.parameters.scale_variable_names = names
 
-    def __scale_variables_from_dict(self, scaling_param: dict[int|float, Any]):
-        scale_variables = []
-        for L in scaling_param.keys():
-            scale_variables.append(L)
-        scale_variables.sort()
-        return scale_variables
-
-    def __is_valid_scaling_parameter(self, name: str, new_scaling_param: dict) -> bool:
-        if len(self.parameters.scaling_params) == 0:
-            return True
-        
-        new_length = []
-        for L in new_scaling_param.keys():
-            if not isinstance(L, int) or isinstance(L, float):
-                print(f">> Error \"{name}\" contains non scaling key {L}")
-                return False
-            new_length.append(L)
-            
-        new_length.sort()
-        for key, params in self.parameters.scaling_params.items():
-            length = self.__scale_variables_from_dict(params)
-            
-            if new_length != length:
-                print(f">> Error \"{name}\" does not contain same scaling parameters as {key}")
-                return False
-        return True         
- 
     def set_output_type(self, out_type: Type[OutType]):
         self.experiment.out_type = out_type       
         
     def add_static_parameter(self, key: str, value):
-        print(f">> Succesfully added \"{key}\"")
-        self.parameters.static_params.update({key : value})
+        self.parameters.add_static_parameter(key, value)
         
     def add_scaling_parameter(self, name: str, new_param: dict):
-        if not self.__is_valid_scaling_parameter(name, new_param):
-            return
-        print(f">> Succesfully added \"{name}\"")
-        self.parameters.scaling_params.update({name : new_param})
-        if self.parameters.scale_variables is None:
-            self.parameters.scale_variables = self.__scale_variables_from_dict(new_param)
+        self.parameters.add_scaling_parameter(name, new_param)
 
     def set_static_parameter(self, key, value):
-        try:
-            self.parameters.static_params[key] = value
-            print(f">> \"{key}\" set")
-        except KeyError as _:
-            print(f">> Key \"{key}\" not found")
+        self.parameters.set_static_parameter(key, value)
     
     def set_scaling_parameter(self, key, value):
-        try:
-            self.parameters.scaling_params[key] = value
-            print(f">> \"{key}\" set")
-        except KeyError as _:
-            print(f">> Key \"{key}\" not found")
+        self.parameters.set_scaling_parameter(key, value)
     
     @abstractmethod
     def build(self, load_only = False) -> Any: 
--- src/physsm/experiment_data.py
+++ src/physsm/experiment_data.py
@@ -4,22 +4,120 @@ from typing import Any, TypeVar
 from .experiment_output import ExperimentOutput
 from .runnner import*
 from pathlib import Path
-from dataclasses import dataclass
 
- 
+def log_path(self, path: Path, verbose = False) -> Path:
+    if not self.verbose:
+        return path.relative_to(self.paths_data.proj_dir)
+    return path
 
 class PathData:
-    proj_dir: Path                        = Path()
-    out_paths:   dict[int|float, Path]    = dict()
-    param_paths: dict[int|float, Path]    = dict()
-    target_dir: Path                      = Path()
+    def __init__(self) -> None:
+        self.proj_dir: Path                        = Path()
+        self.out_paths:   dict[int|float, Path]    = dict()
+        self.param_paths: dict[int|float, Path]    = dict()
+        self.target_dir: Path                      = Path()
+    
+    def log_path(self, path: Path, verbose) -> Path:
+        if not verbose:
+            return path.relative_to(self.proj_dir)
+        return path
+    
+    def set_param_path(self, key, path_name: str, verbose: bool):       
+        self.param_paths[key] =  self.target_dir.joinpath(path_name)
+        print(f"-- setting paramfile \"{self.log_path(self.param_paths[key], verbose)}\"")
+        
+    def set_out_path(self, key, out_path_name: str, verbose: bool):       
+        self.out_paths[key] = self.target_dir.joinpath(out_path_name)
+        print(f"-- setting outfile \"{self.log_path(self.out_paths[key], verbose)}\"")
+
+    def set_proj_dir(self, proj_dir):
+        self.proj_dir = proj_dir
+        
+    def set_target_dir(self, results_dir: str, exp_name: str, verbose = False):
+           
+        _results_dir = self.proj_dir.joinpath(results_dir)
+        target_dir   = _results_dir.joinpath(exp_name)
+        if not self.proj_dir.exists():
+            raise NotADirectoryError(f">> Project Directory \"{self.log_path(self.proj_dir, verbose)}\" Not Found.")
+        else:
+            if not verbose:
+                print(f">> Project Directory \"{self.proj_dir.name}\" Found.")
+            else:
+                print(f">> Project Directory \"{self.proj_dir}\" Found.")
+                
+        if target_dir.exists():
+            print(f">> Directory \"{self.log_path(target_dir, verbose)}\" Found.")
+        else:
+            target_dir.mkdir(parents=True)
+            print(f">> Directory \"{self.log_path(target_dir, verbose)}\" created.")
+
+        self.target_dir = target_dir
+      
+
 
 class Parameters:
-    scale_variables: list|np.ndarray|None = None
-    scale_variable_names: list[str]       = ["L"]  
-    static_params:  dict[str, Any]        = dict()
-    scaling_params: dict[str, dict]       = dict()
+    def __init__(self) -> None:
+        self.scale_variables: list|np.ndarray|None = None
+        self.scale_variable_names: list[str]       = ["L"]  
+        self.static_params:  dict[str, Any]        = dict()
+        self.scaling_params: dict[str, dict]       = dict()
+        
+    @staticmethod
+    def get_scale_variables_from_dict(scaling_param: dict[int|float, Any]):
+        scale_variables = []
+        for L in scaling_param.keys():
+            scale_variables.append(L)
+        scale_variables.sort()
+        return scale_variables
+
+    def compare_new_scaling_param(self, name: str, new_scaling_param: dict) -> bool:
+        new_lengths = Parameters.get_scale_variables_from_dict(new_scaling_param) 
+        
+        for key, params in self.scaling_params.items():
+            lengths = Parameters.get_scale_variables_from_dict(params)
+            if new_lengths != lengths:
+                ValueError(f"\"{name}\" does not contain same scaling parameters as {key}")
+        return True
     
+    def valididate_scaling_parameter(self, name: str, new_scaling_param: dict):
+       
+        for L in new_scaling_param.keys():
+            if not isinstance(L, int) or isinstance(L, float):
+                raise ValueError(f"\"{name}\" contains non scaling key {L}")
+        if self.scaling_params is None or len(self.scaling_params) == 0:
+            return 
+        self.compare_new_scaling_param(name, new_scaling_param)
+
+    def add_scaling_parameter(self, name: str, new_scaling_param: dict):
+        try:
+            self.valididate_scaling_parameter(name, new_scaling_param)
+        except Exception as e:
+            raise ValueError(f"add_scaling_parameter() error {e.args}")
+        
+        print(f">> Succesfully added \"{name}\"")
+        self.scaling_params.update({name : new_scaling_param})
+        
+        if self.scale_variables is None:
+            self.scale_variables = Parameters.get_scale_variables_from_dict(new_scaling_param)
+    
+    def add_static_parameter(self, key: str, value):
+        print(f">> Succesfully added \"{key}\"")
+        self.static_params.update({key : value})
+    
+    def set_static_parameter(self, key, value):
+        try:
+            self.static_params[key] = value
+            print(f">> \"{key}\" set")
+        except KeyError as _:
+            print(f">> Key \"{key}\" not found")
+    
+    def set_scaling_parameter(self, key, value):
+        try:
+            self.scaling_params[key] = value
+            print(f">> \"{key}\" set")
+        except KeyError as _:
+            print(f">> Key \"{key}\" not found")
+            
 OutType =  TypeVar("OutType", bound= ExperimentOutput,) 
 
 class BaseExperimentData[OutType]:
@@ -32,13 +130,13 @@ class BaseExperimentData[OutType]:
         self.out_key                       = "outputfile"
 
     def copy_data(self, exp_data: BaseExperimentData):
-        self.verbose_log          = exp_data.verbose_log
-        self.paths_data           = exp_data.paths_data
-        self.paths_data           = self.paths_data 
-        self.parameters           = exp_data.parameters 
-        self.out_type             = exp_data.out_type
-        self.runner               = exp_data.runner
-        self.out_key              = exp_data.out_key
+        self.verbose_log   = exp_data.verbose_log
+        self.paths_data    = exp_data.paths_data
+        self.paths_data    = self.paths_data 
+        self.parameters    = exp_data.parameters 
+        self.out_type      = exp_data.out_type
+        self.runner        = exp_data.runner
+        self.out_key       = exp_data.out_key
 
     
 if __name__ == "__main__":
